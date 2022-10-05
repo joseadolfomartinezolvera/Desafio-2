e90e253e76a4052f8065ed4bfbfb0d0cba83dff4
joseadolfomartinezolvera <56425103+joseadolfomartinezolvera@users.noreply.github.com>
2022-10-04 20:46:43 -0500

    Update DestroyOutOfBoundsX.cs

--- Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = -30;
+    private float leftLimit = -30;//se cambio su valor a negativo 
     private float bottomLimit = -5;
 
     
     void Update()
     {
+        //en esta parte se hizo que leftLimit fuera negativo, se cambio el valor > de la posicion x por un < 
+        //y se cambio z por y en el limite inferios
         // Destruir perros si la posición x es inferior al límite izquierdo
         if (transform.position.x < leftLimit)
         {
